3f5c4cc5320d1797cfc4c22a0ce52b9ab3ff909c
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-09-05 12:00:56 -0400

    About Section complete

--- react-portfolio-website/src/components/about/About.jsx
+++ react-portfolio-website/src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './about.css'
+import ME from '../../images/me2.jpeg'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return (
-    <div>About</div>
+   <section id='about'>
+    <h5>Get To Know</h5>
+    <h2>About Me</h2>
+
+    <div className='container about__container'>
+      <div className='about__me'>
+        <div className="about__me-image">
+          <img src={ME} alt="About Image"></img>
+        </div>
+      </div>
+      <div className="about__content">
+        <div className="about__cards">
+          <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>1 Year</small>
+          </article>
+
+          <article className='about__card'>
+            <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small>Completed Projects</small>
+          </article>
+
+          <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>1 Year</small>
+          </article>
+        </div>
+
+        <p>
+          I am a Software Engineer with a passion for Design!
+        </p>
+
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+      </div>
+    </div>
+   </section>
   )
 }
 
--- react-portfolio-website/src/components/nav/Nav.jsx
+++ react-portfolio-website/src/components/nav/Nav.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBookBookmark} from 'react-icons/bi'
+import {RiMessage2Line} from 'react-icons/ri'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'
+      onClick={() => setActiveNav('#')}
+      className={activeNav === '#' ? 'active' : ''}><AiOutlineHome />
+      </a>
+      <a href='#about' 
+      onClick={() => setActiveNav ('#about')}
+      className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href='#experience'
+      onClick={() => setActiveNav ('#experience')}
+      className={activeNav === '#experience' ? 'active' : ''}><BiBookBookmark /></a>
+      {/* <a href='#projects'><HiOutlineRectangleGroup /></a> */}
+      <a href='#contact'
+      onClick={() => setActiveNav ('#contact')}
+      className={activeNav === '#contact' ? 'active' : ''}><RiMessage2Line /></a>
+    </nav>
   )
 }
 
