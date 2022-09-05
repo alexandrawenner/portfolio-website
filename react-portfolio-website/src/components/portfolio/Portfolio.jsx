e0235d359f1de49011e85778894cf0dece35ade1
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-09-05 14:18:52 -0400

    more portfolio set up. Still need screenshots of apps and a demo for phase 4 project

--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -12,9 +12,36 @@ const Portfolio = () => {
           <div className='portfolio_item-image'>
 
           </div>
-          <h3>Portfolio Item Title</h3>
+          <h3>Broadway Musical Hub</h3>
           <a href="https://github.com/alexandrawenner" className='btn' target='_blank'>Github</a>
           <a href="https://www.linkedin.com/posts/alexandra-wenner_softwareengineering-capstoneproject-flatiron-activity-6969409376784654336-Q8Ft?utm_source=share&utm_medium=member_desktop" className='btn' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio_item-image'>
+
+          </div>
+          <h3>Spotify Clone</h3>
+          <a href="https://github.com/alexandrawenner" className='btn' target='_blank'>Github</a>
+          <a href="https://www.linkedin.com/posts/alexandra-wenner_softwareengineering-capstoneproject-flatiron-activity-6969409376784654336-Q8Ft?utm_source=share&utm_medium=member_desktop" className='btn' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio_item-image'>
+
+          </div>
+          <h3>A24 Flix</h3>
+          <a href="https://github.com/alexandrawenner" className='btn' target='_blank'>Github</a>
+          <a href="https://www.linkedin.com/posts/alexandra-wenner_ruby-fullstackdevelopment-flatiron-activity-6960009491044016128-6f9G?utm_source=share&utm_medium=member_desktop" className='btn' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio_item-image'>
+
+          </div>
+          <h3>Task App</h3>
+          <a href="https://github.com/alexandrawenner" className='btn' target='_blank'>Github</a>
+          <a href="https://www.linkedin.com/posts/alexandra-wenner_react-project-flatiron-activity-6960007593662500865-eV1y?utm_source=share&utm_medium=member_desktop" className='btn' target='_blank'>Live Demo</a>
         </article>
       </div>
     </section>
