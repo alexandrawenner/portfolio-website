faf535e9ab5496ba7290cf05163b9aec91471a66
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-09-05 14:11:37 -0400

    beginning portfolio section

--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,21 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio_item-image'>
+
+          </div>
+          <h3>Portfolio Item Title</h3>
+          <a href="https://github.com/alexandrawenner" className='btn' target='_blank'>Github</a>
+          <a href="https://www.linkedin.com/posts/alexandra-wenner_softwareengineering-capstoneproject-flatiron-activity-6969409376784654336-Q8Ft?utm_source=share&utm_medium=member_desktop" className='btn' target='_blank'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
