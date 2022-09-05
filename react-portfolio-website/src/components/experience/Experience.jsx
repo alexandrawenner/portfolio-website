3c21be48bd8877e36c3f6d1953fd690b6247aa07
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-09-05 12:10:35 -0400

    addded experience

--- react-portfolio-website/src/components/experience/Experience.jsx
+++ react-portfolio-website/src/components/experience/Experience.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Contact</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience __container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className="experience__content"></div>
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>HTML</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>CSS</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>JavaScript</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>React.js</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+        </div>
+        <div className='experience__backend'></div>
+          <h3>Backend Developemt</h3>
+          <div className="experience__content"></div>
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>Ruby on Rails</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>SQL</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>Active Record</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+
+          <article className='experience__details'>
+            <BsFillPatchCheckFill />
+            <h4>Sinatra</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+      </div>
+    </section>
   )
 }
 
