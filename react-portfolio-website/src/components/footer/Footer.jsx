354c8651973670bbf50bff8759002ce25d78b18d
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-09-11 15:18:00 -0400

    footer is done

--- react-portfolio-website/src/components/footer/Footer.jsx
+++ react-portfolio-website/src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'></a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://www.linkedin.com/in/alexandra-wenner/"><BsLinkedin /></a>
+        <a href="https://github.com/alexandrawenner"><BsGithub /></a>
+      </div>
+
+      {/* <div className='footer__copyright'>
+        <small>&copy; </small>
+      </div> */}
+
+    </footer>
   )
 }
 
