4847e4d744d3d954e0a2ccb6ead0a25eb5d69cce
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-09-12 12:49:46 -0400

    added website visit feature

--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -11,28 +11,32 @@ const data = [
     image: IMG4,
     title: 'Broadway Hub',
     github: 'https://github.com/alexandrawenner/Capstone-Project',
-    demo: 'https://www.linkedin.com/posts/alexandra-wenner_softwareengineering-capstoneproject-flatiron-activity-6969409376784654336-Q8Ft?utm_source=share&utm_medium=member_desktop'
+    demo: 'https://www.linkedin.com/posts/alexandra-wenner_softwareengineering-capstoneproject-flatiron-activity-6969409376784654336-Q8Ft?utm_source=share&utm_medium=member_desktop',
+    website: ''
   },
   {
     id: 2,
     image: IMG3,
     title: 'Nu Vibration',
     github: 'https://github.com/alexandrawenner/nu-vibration-app',
-    demo: 'https://www.linkedin.com/posts/victorzarour_softwareengineer-developer-reactjs-activity-6961407246501089280-ofIK?utm_source=share&utm_medium=member_desktop'
+    demo: 'https://www.linkedin.com/posts/victorzarour_softwareengineer-developer-reactjs-activity-6961407246501089280-ofIK?utm_source=share&utm_medium=member_desktop',
+    website: 'https://releasethewiggle.herokuapp.com/'
   },
   {
     id: 3,
     image: IMG2,
-    title: 'A24 Flix',
+    title: 'Indie Hub',
     github: 'https://github.com/alexandrawenner/my-app-frontend',
-    demo: 'https://www.linkedin.com/posts/alexandra-wenner_ruby-fullstackdevelopment-flatiron-activity-6960009491044016128-6f9G?utm_source=share&utm_medium=member_desktop'
+    demo: 'https://www.linkedin.com/posts/alexandra-wenner_ruby-fullstackdevelopment-flatiron-activity-6960009491044016128-6f9G?utm_source=share&utm_medium=member_desktop',
+    website: 'https://indie-hub.herokuapp.com/'
   },
   {
     id: 4,
     image: IMG1,
     title: 'Task Daily',
     github: 'https://github.com/alexandrawenner/productivity-app-attempt2',
-    demo: 'https://www.linkedin.com/posts/alexandra-wenner_react-project-flatiron-activity-6960007593662500865-eV1y?utm_source=share&utm_medium=member_desktop'
+    demo: 'https://www.linkedin.com/posts/alexandra-wenner_react-project-flatiron-activity-6960007593662500865-eV1y?utm_source=share&utm_medium=member_desktop',
+    website: ''
   },
 ]
 
@@ -44,7 +48,7 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, github, demo, website}) => {
             return(
               <article className='portfolio__item'>
                 <div className='portfolio__item-image'>
@@ -54,6 +58,7 @@ const Portfolio = () => {
                 <div className='portfolio__item-cta'>
                   <a href={github} className='btn' target='_blank'>Github</a>
                   <a href={demo} className='btn' target='_blank'>Live Demo</a>
+                  <a href={website} className='btn' target='_blank'>Visit Website</a>
                 </div>
               </article>
             )
